18b436fb5e5000e9ec06df7e7de726bee6f9dd70
Siva Brahmam <Siva Brahmam@LAPTOP-K97F6E3E>
2023-06-29 15:43:52 +0530

    this is my 2nd commit

---
+++ src/test/java/realtime/firstprogram.java
@@ -0,0 +1,35 @@
+package realtime;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class firstprogram {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.facebook.com");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		String title=driver.getTitle();
+		System.out.println(title);
+		String url=driver.getCurrentUrl();
+		System.out.println(url);
+		String pagesource=driver.getPageSource();
+		System.out.println(pagesource);
+		String windowhandle=driver.getPageSource();
+		System.out.println(windowhandle);
+		
+		
+	}
+
+}
